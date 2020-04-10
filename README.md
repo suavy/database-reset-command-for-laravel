59a8a693cc11b6f218da9ccff90369df64ef174d
Christophe <cdo_outeiro@me.com>
2020-04-11 00:29:17 +0200

    Add --force option

--- README.md
+++ README.md
@@ -11,25 +11,15 @@ This is where your description should go. Try and limit it to a paragraph or two
 
 You can install the package via composer:
 
-```bash
+``` bash
 composer require suavy/database-reset-command-for-laravel
 ```
 
 ## Usage
 
-``` php
-// Usage description here
-```
-
-### Testing
-
 ``` bash
-composer test
+php artisan db:reset
 ```
-
-### Changelog
-
-Please see [CHANGELOG](CHANGELOG.md) for more information what has changed recently.
 
 ## Contributing
 
@@ -37,7 +27,7 @@ Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
 
 ### Security
 
-If you discover any security related issues, please email cdo_outeiro@me.com instead of using the issue tracker.
+If you discover any security related issues, please email us instead of using the issue tracker.
 
 ## Credits
 
--- src/app/Commands/DatabaseReset.php
+++ src/app/Commands/DatabaseReset.php
@@ -7,62 +7,52 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class DatabaseReset extends Command
-{
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'db:reset';
+class DatabaseReset extends Command {
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
+    protected $signature = 'db:reset
+                            {--F|force : Command will launch with no warning and no production protection}';
+
     protected $description = 'Import database & migrate';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
+    private $databaseName;
+    private $databasePassword;
+    private $databaseUsername;
+
     public function __construct()
     {
         parent::__construct();
+        $this->databaseName = env('DB_DATABASE');
+        $this->databasePassword = env('DB_PASSWORD');
+        $this->databaseUsername = env('DB_USERNAME');
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return void
-     */
     public function handle() {
-        if((env('APP_ENV') != 'prod') && (env('APP_ENV') != 'production')) {
+        $force = $this->option('force');
+        if($force || ((env('APP_ENV') != 'prod') && (env('APP_ENV') != 'production'))) {
             $this->warn('This command import the file "db.sql" (if exist) and launch migrations. All recent data will be deleted.');
-            if($this->confirm('Are you sure ?')) {
-                $databaseName = env('DB_DATABASE');
-                $databasePassword = env('DB_PASSWORD');
-                $databaseUsername = env('DB_USERNAME');
-                $this->info('Dropping database...');
-                exec("mysql --user=$databaseUsername --password=$databasePassword -e 'DROP DATABASE $databaseName;'");
-                $this->info('Database dropped');
-                $this->info('Creating database...');
-                exec("mysql --user=$databaseUsername --password=$databasePassword -e 'CREATE DATABASE $databaseName;'");
-                $this->info('Database created');
-                //$this->info('Importing database...');
-                //exec("mysql --user=$databaseUsername --password=$databasePassword $databaseName < db.sql");
-                //$this->info('Database imported');
-                $this->info('Migrating database...');
-                Artisan::call('migrate');
-                $this->info(Artisan::output());
-                $this->info('Database migrated');
+            if($force || $this->confirm('Are you sure ?')) {
+                $this->resetDatabase();
             }
         } else {
-            $this->alert('This command can\'t be used on production env.');
-            $this->alert('If you still want to do it, please change your APP_ENV to anything else than "prod" or "production".');
+            $this->alert('This command can\'t be used on production.');
+            $this->alert('If you still want to use it, use option --force (--F shortcut).');
         }
+    }
+
+    public function resetDatabase() {
+        $this->info('Dropping database...');
+        exec("mysql --user=$this->databaseUsername --password=$this->databasePassword -e 'DROP DATABASE $this->databaseName;'");
+        $this->info('Database dropped');
+        $this->info('Creating database...');
+        exec("mysql --user=$this->databaseUsername --password=$this->databasePassword -e 'CREATE DATABASE $this->databaseName;'");
+        $this->info('Database created');
+        //$this->info('Importing database...');
+        //exec("mysql --user=$databaseUsername --password=$databasePassword $databaseName < db.sql");
+        //$this->info('Database imported');
+        $this->info('Migrating database...');
+        Artisan::call('migrate');
+        $this->info(Artisan::output());
+        $this->info('Database migrated');
     }
 
 }
